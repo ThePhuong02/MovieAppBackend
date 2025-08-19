6c0ce002e4950a12e6ab50963f48d9c93866d529
ng.phuoc_19 <phuocnn@cyberk.io>
2025-08-19 21:44:30 +0700

    “update”

--- src/main/java/movieapp/webmovie/service/FavoriteService.java
+++ src/main/java/movieapp/webmovie/service/FavoriteService.java
@@ -4,6 +4,7 @@ import movieapp.webmovie.entity.*;
 import movieapp.webmovie.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -18,6 +19,7 @@ public class FavoriteService {
         }
     }
 
+    @Transactional
     public void removeFavorite(Long userId, Long movieId) {
         favoriteRepo.deleteByUserIdAndMovieId(userId, movieId);
     }
